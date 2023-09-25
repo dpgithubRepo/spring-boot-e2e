81c4101dc24767c9f35bfe4a50dec921a3af21e5
Durga Prasad <durga.pras10@gmail.com>
2023-09-25 19:04:13 +0530

    initial commit

---
+++ product-service/src/test/java/com/dp/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
